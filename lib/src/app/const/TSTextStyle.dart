52f48d1f2e0d5192f7f6eaa5d5c1bef39861b18d
Mustafa Turkmen <mustafa.muhammed@dop.com.tr>
2022-04-26 14:55:06 +0300

    Update TSTextStyle.dart

--- lib/src/app/const/TSTextStyle.dart
+++ lib/src/app/const/TSTextStyle.dart
@@ -12,6 +12,11 @@ TextStyle s10W400Dark(BuildContext context) => Theme.of(context).textTheme.bodyT
 TextStyle s10W500Dark(BuildContext context) => Theme.of(context).textTheme.bodyText2!.copyWith(fontWeight: FontWeight.w500);
 TextStyle s10W700Dark(BuildContext context) => Theme.of(context).textTheme.bodyText2!.copyWith(fontWeight: FontWeight.w700);
 
+TextStyle s11W300Dark(BuildContext context) => Theme.of(context).textTheme.caption!.copyWith(fontWeight: FontWeight.w300);
+TextStyle s11W400Dark(BuildContext context) => Theme.of(context).textTheme.caption!;
+TextStyle s11W500Dark(BuildContext context) => Theme.of(context).textTheme.caption!.copyWith(fontWeight: FontWeight.w500);
+TextStyle s11W700Dark(BuildContext context) => Theme.of(context).textTheme.caption!.copyWith(fontWeight: FontWeight.w700);
+
 TextStyle s12W300Dark(BuildContext context) => Theme.of(context).textTheme.headline1!.copyWith(fontWeight: FontWeight.w300);
 TextStyle s12W400Dark(BuildContext context) => Theme.of(context).textTheme.headline1!;
 TextStyle s12W500Dark(BuildContext context) => Theme.of(context).textTheme.headline1!.copyWith(fontWeight: FontWeight.w500);
@@ -22,30 +27,32 @@ TextStyle s13W400Dark(BuildContext context) => Theme.of(context).textTheme.headl
 TextStyle s13W500Dark(BuildContext context) => Theme.of(context).textTheme.headline2!.copyWith(fontWeight: FontWeight.w500);
 TextStyle s13W700Dark(BuildContext context) => Theme.of(context).textTheme.headline2!.copyWith(fontWeight: FontWeight.w700);
 
-TextStyle s14W300Dark(BuildContext context) => Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w300);
-TextStyle s14W400Dark(BuildContext context) => Theme.of(context).textTheme.bodyText1!;
-TextStyle s14W500Dark(BuildContext context) => Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w500);
-TextStyle s14W700Dark(BuildContext context) => Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w700);
+TextStyle s14W300Dark(BuildContext context) => Theme.of(context).textTheme.headline3!.copyWith(fontWeight: FontWeight.w300);
+TextStyle s14W400Dark(BuildContext context) => Theme.of(context).textTheme.headline3!;
+TextStyle s14W500Dark(BuildContext context) => Theme.of(context).textTheme.headline3!.copyWith(fontWeight: FontWeight.w500);
+TextStyle s14W700Dark(BuildContext context) => Theme.of(context).textTheme.headline3!.copyWith(fontWeight: FontWeight.w700);
 
-TextStyle s16W300Dark(BuildContext context) => Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w300);
-TextStyle s16W400Dark(BuildContext context) => Theme.of(context).textTheme.subtitle1!;
-TextStyle s16W500Dark(BuildContext context) => Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w500);
-TextStyle s16W700Dark(BuildContext context) => Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w700);
+TextStyle s16W300Dark(BuildContext context) => Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w300);
+TextStyle s16W400Dark(BuildContext context) => Theme.of(context).textTheme.bodyText1!;
+TextStyle s16W500Dark(BuildContext context) => Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w500);
+TextStyle s16W700Dark(BuildContext context) => Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.w700);
 
-TextStyle s18W300Dark(BuildContext context) => Theme.of(context).textTheme.headline3!.copyWith(fontWeight: FontWeight.w300);
-TextStyle s18W400Dark(BuildContext context) => Theme.of(context).textTheme.headline3!;
-TextStyle s18W500Dark(BuildContext context) => Theme.of(context).textTheme.headline3!.copyWith(fontWeight: FontWeight.w500);
-TextStyle s18W700Dark(BuildContext context) => Theme.of(context).textTheme.headline4!;
+TextStyle s15W300Dark(BuildContext context) => Theme.of(context).textTheme.headline4!.copyWith(fontWeight: FontWeight.w300);
+TextStyle s15W400Dark(BuildContext context) => Theme.of(context).textTheme.headline4!;
+TextStyle s15W500Dark(BuildContext context) => Theme.of(context).textTheme.headline4!.copyWith(fontWeight: FontWeight.w500);
+TextStyle s15W700Dark(BuildContext context) => Theme.of(context).textTheme.headline4!.copyWith(fontWeight: FontWeight.w700);
+
+TextStyle s18W300Dark(BuildContext context) => Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w300);
+TextStyle s18W400Dark(BuildContext context) => Theme.of(context).textTheme.subtitle1!;
+TextStyle s18W500Dark(BuildContext context) => Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w500);
+TextStyle s18W700Dark(BuildContext context) => Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w700);
 
 TextStyle s20W300Dark(BuildContext context) => Theme.of(context).textTheme.headline5!.copyWith(fontWeight: FontWeight.w300);
 TextStyle s20W400Dark(BuildContext context) => Theme.of(context).textTheme.headline5!;
 TextStyle s20W500Dark(BuildContext context) => Theme.of(context).textTheme.headline5!.copyWith(fontWeight: FontWeight.w500);
 TextStyle s20W700Dark(BuildContext context) => Theme.of(context).textTheme.headline5!.copyWith(fontWeight: FontWeight.w700);
 
-
 TextStyle s26W300Dark(BuildContext context) => Theme.of(context).textTheme.headline6!.copyWith(fontWeight: FontWeight.w300);
 TextStyle s26W400Dark(BuildContext context) => Theme.of(context).textTheme.headline6!;
 TextStyle s26W500Dark(BuildContext context) => Theme.of(context).textTheme.headline6!.copyWith(fontWeight: FontWeight.w500);
 TextStyle s26W700Dark(BuildContext context) => Theme.of(context).textTheme.headline6!.copyWith(fontWeight: FontWeight.w700);
-
-
