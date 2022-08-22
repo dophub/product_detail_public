e6d0da0a08563dee0098b7a8fb03ac3f18ba64b8
Mustafa Turkmen <49743631+Mto292@users.noreply.github.com>
2022-08-22 14:33:17 +0300

    fix(UI)

--- lib/src/app/const/assets.dart
+++ lib/src/app/const/assets.dart
@@ -1,6 +1,6 @@
 /// Tüm Assets path leri buradan çekilecek Proje içinde Herhangi bir path yazılmayacak
 
-String assets = 'assets/images/other/';
+String assets = 'assets/images/';
 
 String arrowIcon = assets + 'arrow_icon.svg';
 
